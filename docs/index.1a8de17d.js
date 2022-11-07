697c917c390ad09f82be097349b7ee166cd6ff46
James-alderson <james.alderson.a@gmail.com>
2022-11-07 10:44:29 +0330

    Rename 'dist' directory to 'docs'

--- docs/index.1a8de17d.js
+++ docs/index.1a8de17d.js
@@ -1,2 +1,2 @@
 document.querySelector("header").innerHTML="\n    <h1>Learning parcel and babel</h1>\n    <p>\n      Parcel is module bundler<br>\n      Babel is js transcompailer\n    </p>\n  ";
-//# sourceMappingURL=index.19e285cc.js.map
+//# sourceMappingURL=index.1a8de17d.js.map
